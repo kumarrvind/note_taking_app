e037ca98f74de740e8eefdd589160f2d485353d2
Arvind Kumar <108170328+kumarrvind@users.noreply.github.com>
2022-07-09 01:59:23 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Notes from "./Notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Notes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1> This is me Arvind Kumar</h1>
+      <p>
+        studying in IIT(ISM) DHANBAD, persuing bachelor of technology in Mineral
+        and Metallurgical Engineering.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
